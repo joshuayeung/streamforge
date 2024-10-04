81d8b1c2022f6561d5292e4dde92a4c6a1586532
Joshua Yeung <joshua.chyeung@gmail.com>
2024-10-04 06:35:46 +0000

    feat: make msk public assessible

--- terraform/msk/outputs.tf
+++ terraform/msk/outputs.tf
@@ -1,3 +1,8 @@
 output "cluster_arn" {
   value = aws_msk_cluster.kafka_cluster.arn
 }
+
+# Output the MSK broker endpoints
+output "broker_endpoints" {
+  value = aws_msk_cluster.kafka_cluster.bootstrap_brokers
+}
