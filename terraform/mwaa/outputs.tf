b0bba3e7124959ab8f71d31e808db54222ee08d8
Joshua Yeung <joshua.chyeung@gmail.com>
2024-10-07 04:36:44 +0000

    feat: update mwaa tf to use default values

--- terraform/mwaa/outputs.tf
+++ terraform/mwaa/outputs.tf
@@ -1,7 +1,39 @@
-output "environment_arn" {
-  value = aws_mwaa_environment.airflow.arn
+output "aws_s3_bucket_name" {
+  description = "S3 bucket Name of the MWAA Environment"
+  value       = module.mwaa.aws_s3_bucket_name
 }
 
-output "mwaa_dag_bucket" {
-  value = aws_s3_bucket.mwaa_dag_bucket.id
+output "mwaa_arn" {
+  description = "The ARN of the MWAA Environment"
+  value       = module.mwaa.mwaa_arn
+}
+
+output "mwaa_role_arn" {
+  description = "The ARN of the MWAA Environment"
+  value       = module.mwaa.mwaa_role_arn
+}
+
+output "mwaa_role_name" {
+  description = "IAM role name of the MWAA Environment"
+  value       = module.mwaa.mwaa_role_name
+}
+
+output "mwaa_security_group_id" {
+  description = "The ARN of the MWAA Environment"
+  value       = module.mwaa.mwaa_security_group_id
+}
+
+output "mwaa_service_role_arn" {
+  description = "The Service Role ARN of the Amazon MWAA Environment"
+  value       = module.mwaa.mwaa_service_role_arn
+}
+
+output "mwaa_status" {
+  description = " The status of the Amazon MWAA Environment"
+  value       = module.mwaa.mwaa_status
+}
+
+output "mwaa_webserver_url" {
+  description = "The webserver URL of the MWAA Environment"
+  value       = module.mwaa.mwaa_webserver_url
 }
