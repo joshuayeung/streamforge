9cb68af441dcd72cbc2393d249a0065b965b9a07
Joshua Yeung <joshua.chyeung@gmail.com>
2024-10-07 09:32:21 +0000

    cicd: must use static name

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "<YOUR_ORGANIZATION_NAME>" # Replace with your organization name
+    organization = "Pythogenius" # Replace with your organization name
 
     workspaces {
-      name = "<YOUR_WORKSPACE_NAME>" # Replace with your workspace name
+      name = "streamforge" # Replace with your workspace name
     }
   }
 }
