cb5a7816f03de7629ff18de55f2c70b945b1ae32
Joshua Yeung <joshua.chyeung@gmail.com>
2024-10-09 06:38:15 +0000

    refactor: remove unnecessary var and add requirements.txt

--- terraform/mwaa/variables.tf
+++ terraform/mwaa/variables.tf
@@ -1,6 +1,6 @@
 variable "name" {
   description = "Name of MWAA Environment"
-  default     = "basic-mwaa"
+  default     = "streamforge"
   type        = string
 }
 
@@ -20,4 +20,10 @@ variable "vpc_cidr" {
   description = "VPC CIDR for MWAA"
   type        = string
   default     = "10.1.0.0/16"
+}
+
+variable "requirements_s3_path" {
+  description = "(Optional) The relative path to the requirements.txt file on your Amazon S3 storage bucket. For example, requirements.txt. If a relative path is provided in the request, then requirements_s3_object_version is required."
+  type        = string
+  default     = "dags/requirements.txt"
 }
